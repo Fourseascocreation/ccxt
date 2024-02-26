cfeac2633469d9d08a539bdf3d25cad040fae321
Sam Germain <sam.germain@usask.ca>
2024-02-26 06:21:33 -0500

    binance.has update

--- ts/src/binance.ts
+++ ts/src/binance.ts
@@ -48,7 +48,7 @@ export default class binance extends Exchange {
                 'createMarketSellOrderWithCost': true,
                 'createOrder': true,
                 'createOrders': true,
-                'createOrderWithTakeProfitAndStopLoss': true,
+                'createOrderWithTakeProfitAndStopLoss': false,
                 'createPostOnlyOrder': true,
                 'createReduceOnlyOrder': true,
                 'createStopLimitOrder': true,
