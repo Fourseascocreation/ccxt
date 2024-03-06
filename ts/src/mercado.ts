66639422fe255f70f734f70cc7d5bb1169f8cef3
Sam Germain <sam.germain@usask.ca>
2024-03-05 19:54:07 -0500

    mercado.createOrder parseToNumeric used

--- ts/src/mercado.ts
+++ ts/src/mercado.ts
@@ -470,7 +470,7 @@ export default class mercado extends Exchange {
                 }
                 const amountString = this.numberToString (amount);
                 const priceString = this.numberToString (price);
-                const cost = this.parseNumber (Precise.stringMul (amountString, priceString));
+                const cost = this.parseToNumeric (Precise.stringMul (amountString, priceString));
                 request['cost'] = this.priceToPrecision (market['symbol'], cost);
             } else {
                 request['quantity'] = this.amountToPrecision (market['symbol'], amount);
